d77ed8989134c0ebf327734799a7b62533e7e97a
jeheecheon <jeheecheon@gmail.com>
2025-10-18 01:47:26 +0900

    feat: implement HomeBackground

---
+++ src/components/HomeBackground.tsx
@@ -0,0 +1,29 @@
+import ImageWithPlaceholder from "@/components/ImageWithPlaceholder";
+import { cn } from "@/utils/styles";
+import { type FC } from "react";
+
+type Props = {
+  className?: string;
+};
+
+const HomeBackground: FC<Props> = ({ className }) => {
+  return (
+    <div className={cn("h-200", className)}>
+      <ImageWithPlaceholder
+        className="h-200 opacity-15"
+        imgClassName="object-cover object-center"
+        height={1536}
+        width={1024}
+        quality={80}
+        src="/images/home-background.png"
+        alt="Home Background"
+      />
+      <div
+        className="absolute inset-0 bg-gradient-to-b from-transparent to-gray-950"
+        role="presentation"
+      />
+    </div>
+  );
+};
+
+export default HomeBackground;
