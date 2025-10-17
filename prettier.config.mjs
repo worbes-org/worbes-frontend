df2df26cff31d79197c0c82f6339b016f6bcdfb2
jeheecheon <jeheecheon@gmail.com>
2025-10-18 01:45:58 +0900

    chore: set up to format tw classes

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -4,6 +4,7 @@
 const config = {
   tabWidth: 2,
 
+  tailwindAttributes: ["className", "class", "imgClassName"],
   tailwindStylesheet: "./src/styles/globals.css",
   plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss"],
 };
