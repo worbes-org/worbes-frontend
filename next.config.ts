9c9c64b666f07d7493aedc75e9104baca9e53a02
jeheecheon <jeheecheon@gmail.com>
2025-10-23 01:37:03 +0900

    feat: configure API URL and set up rewrites for API requests

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const API_URL =
+  process.env.API_URL ??
+  "http://worbes-server-alb-281547945.ap-northeast-2.elb.amazonaws.com";
+
+const nextConfig: NextConfig = {
+  env: {
+    API_URL,
+  },
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_URL}/api/v1/:path*`,
+      },
+    ];
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 
